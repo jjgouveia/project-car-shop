5524b52c4cdd9f804480e14fd256f154c5b3b6c8
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-01-13 15:27:14 -0300

    Projeto Car Shop Iniciado

--- src/Models/Connection.ts
+++ src/Models/Connection.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import console from 'console';
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
-) => mongoose.connect(mongoDatabaseURI);
+) => mongoose.connect(mongoDatabaseURI).then(() => console.log('Database server running'));
 
 export default connectToDatabase;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,39 @@
-import express from 'express';
+import console from 'console';
+import * as express from 'express';
+import ErrorHandler from './middlewares/ErrorHandler';
 
-const app = express();
+class App {
+  public app: express.Express;
+    
+  constructor() {
+    this.app = express.default();
 
-export default app;
+    this.config();
+
+    this.app.get('/doodly', (req, res) => res.json(
+      { message: 'We\'re On Fire' },
+    ));
+  }
+  
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
+    this.app.use(ErrorHandler.handle);
+  }
+  
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export default App;
